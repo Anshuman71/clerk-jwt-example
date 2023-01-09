3439b14ebf4b7e25858280dd4d85ef0c47ebf0a2
Anshuman Bhardwaj <anshu5074@gmail.com>
2023-01-09 11:15:14 +0530

    add clerk sdk

---
+++ pages/api/clerk-safehouse.js
@@ -0,0 +1,15 @@
+import { getAuth } from "@clerk/nextjs/server";
+
+export default async function handler(req, res) {
+  try {
+    const { userId } = getAuth(req);
+    if (!userId) {
+      return res.status(401).json({ message: "error" });
+    }
+    return res
+      .status(200)
+      .json({ safehouseKey: "under-the-doormat", message: "success" });
+  } catch (err) {
+    console.log(err);
+  }
+}
---
+++ pages/safehouse.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+import { SignOutButton, RedirectToSignIn, useUser } from "@clerk/nextjs";
+
+export default function SafeHouse() {
+  const { isSignedIn } = useUser();
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    fetch("/api/clerk-safehouse")
+      .then((res) => res.json())
+      .then((data) => setUserData(data));
+  }, []);
+
+  if (!isSignedIn) {
+    return <RedirectToSignIn />;
+  }
+
+  return (
+    <>
+      <main style={{ padding: "50px" }}>
+        <h1>Safehouse </h1>
+        <p>
+          You Safehour key is{" "}
+          <strong>{userData?.safehouseKey || "Loading..."}</strong>
+        </p>
+        <SignOutButton />
+      </main>
+    </>
+  );
+}
